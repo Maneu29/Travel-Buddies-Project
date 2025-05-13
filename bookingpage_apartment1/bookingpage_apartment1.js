239110ca27c53616d2f4337538b912e29b7b7d51
Maneu29 <jodesseyswet.manisan@hcdc.edu.ph>
2025-05-13 18:57:01 +0800

    Add files via upload

---
+++ bookingpage_apartment1/bookingpage_apartment1.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      alert('You have successfully booked an Apartment accommodation!');
+      window.location.href = '../../apartmentpage/apartments.html'; 
+    });
+  });
