dfc3f767c7193cd0dd7e03cd5b3af6c5d689a696
Maneu29 <jodesseyswet.manisan@hcdc.edu.ph>
2025-05-13 18:57:38 +0800

    Delete bookingpage_flights_international4 directory

--- bookingpage_flights_international4/bookingpage_flights_international4.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('form').addEventListener('submit', function(event) {
-      event.preventDefault();
-      alert('You have successfully booked a Flight!');
-       window.location.href = '../../flightspage/flights.html'; 
-    });
-  });
